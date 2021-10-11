e0c4f37f25d6122b41965d5aedbe98123cbaafaa
Daniela <daniela.leloutre@gmail.com>
2021-10-10 23:48:38 -0300

    config

---
+++ README.md
@@ -0,0 +1,19 @@
+# Microservicio de Administración de Servicios
+[![CI](https://github.com/Ubademy-G3/admin.service/actions/workflows/test.yml/badge.svg)](https://github.com/Ubademy-G3/admin.service/actions/workflows/test.yml)
+[![codecov](https://codecov.io/gh/Ubademy-G3/admin.service/branch/main/graph/badge.svg?token=N90QGLTQ0J)](https://codecov.io/gh/Ubademy-G3/admin.service)
+
+
+# Entorno local
+
+## Requerimientos
+
+* Docker
+* Docker-compose
+
+## Construir y levantar servicios
+
+```docker-compose up -d --build```
+
+## Detener servicios
+
+```docker-compose stop```
---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const { Client } = require("pg");
+
+const app = express();
+
+var client;
+if (process.env.NODE_ENV !== 'stage') {
+    client = new Client({
+      connectionString: process.env.DATABASE_URL,
+      query_timeout: 1000,
+      statement_timeout: 1000,
+      ssl: false
+    }); 
+} else {
+    client = new Client({
+      connectionString: process.env.DATABASE_URL,
+      query_timeout: 1000,
+      statement_timeout: 1000,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    });
+}
+
+client.connect();
+
+app.get("/ping", (req, res) => res.send("Pong!"));
+
+app.get("/status", (req, res) => client.query("SELECT NOW()", (err) => res.send({ service: "UP", db: err ? "DOWN" : "UP" })));
+
+app.listen(process.env.PORT, () => {
+  console.log(`App running on port ${process.env.PORT}`);
+});
