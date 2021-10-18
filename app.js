56117ed164473f31f43cf434b8e7bda2fbc0b815
Daniela <daniela.leloutre@gmail.com>
2021-10-18 16:26:23 -0300

    linter

--- app.js
+++ app.js
@@ -6,23 +6,23 @@ const admin = require("./infrastructure/routes/admin");
 
 const app = express();
 
-var client;
-if (process.env.NODE_ENV !== 'stage') {
-    client = new Client({
-      connectionString: process.env.DATABASE_URL,
-      query_timeout: 1000,
-      statement_timeout: 1000,
-      ssl: false
-    }); 
+let client;
+if (process.env.NODE_ENV !== "stage") {
+  client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    query_timeout: 1000,
+    statement_timeout: 1000,
+    ssl: false,
+  });
 } else {
-    client = new Client({
-      connectionString: process.env.DATABASE_URL,
-      query_timeout: 1000,
-      statement_timeout: 1000,
-      ssl: {
-        rejectUnauthorized: false
-      }
-    });
+  client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    query_timeout: 1000,
+    statement_timeout: 1000,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
 }
 
 client.connect();
@@ -34,5 +34,5 @@ app.get("/ping", (req, res) => res.send("Pong!"));
 app.get("/status", (req, res) => client.query("SELECT NOW()", (err) => res.send({ service: "UP", db: err ? "DOWN" : "UP" })));
 
 app.listen(process.env.PORT, () => {
-  console.log(`App running on port ${process.env.PORT}`);
+  // console.log(`App running on port ${process.env.PORT}`);
 });
